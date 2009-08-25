f334ac97997ae4ab27914d124522279ee66aaab2
Douglass Cutting <cutting@apache.org>
2009-08-25 22:52:55 +0000

    AVRO-104. Permit null fields with Java reflection. Contributed by Eelco Hillenius.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/avro/trunk@807847 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/avro/BarRecord.java
+++ src/test/java/org/apache/avro/BarRecord.java
@@ -32,7 +32,11 @@ public class BarRecord {
  @Override
  public boolean equals(Object that) {
  if (that instanceof BarRecord) {
+ if (this.beerMsg == null) {
+ return ((BarRecord) that).beerMsg == null;
+ } else {
  return this.beerMsg.equals(((BarRecord) that).beerMsg);
+ }
  }
  return false;
  }
