694ac825ba8ed3a5d8105050066c6231e40dde87
Philip Zeyliger <philz@apache.org>
2010-08-23 06:57:37 +0000

    AVRO-613. Create basic frontend to view trace results. Contributed by Patrick Wendell
    git-svn-id: https://svn.apache.org/repos/asf/avro/trunk@988004 13f79535-47bb-0310-9956-ffa450edef68

--- lang/java/src/test/java/org/apache/avro/ipc/trace/TestStaticServlet.java
+++ lang/java/src/test/java/org/apache/avro/ipc/trace/TestStaticServlet.java
@@ -16,31 +16,17 @@
  * limitations under the License.
  */
 
-package org.apache.avro.ipc.stats;
+package org.apache.avro.ipc.trace;
 
-import java.io.IOException;
-import java.net.URL;
+import static org.junit.Assert.*;
 
-import org.mortbay.jetty.servlet.DefaultServlet;
-import org.mortbay.resource.Resource;
+import org.junit.Test;
 
-/**
- * Very simple servlet class capable of serving static files.
- */
-public class StaticServlet extends DefaultServlet {
- public Resource getResource(String pathInContext) {
- // Take only last slice of the URL as a filename, so we can adjust path.
- // This also prevents mischief like '../../foo.css'
- String[] parts = pathInContext.split("/");
- String filename = parts[parts.length - 1];
-
- try {
- URL resource = getClass().getClassLoader().getResource(
- "org/apache/avro/ipc/stats/static/" + filename);
- if (resource == null) { return null; }
- return Resource.newResource(resource);
- } catch (IOException e) {
- return null;
- }
+public class TestStaticServlet {
+ @Test
+ public void testEmptyQueryString() {
+ StaticServlet servlet = new StaticServlet();
+ assertNull(servlet.getResource("/"));
+ assertNull(servlet.getResource("bogusquery"));
  }
 }
