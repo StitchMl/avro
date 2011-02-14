a2eb00553409b40a506a75392c6ba159406a3e54
Scott Carey <scottcarey@apache.org>
2011-02-14 19:10:52 +0000

    AVRO-765. Java: Improvement to BinaryDecoder readLong performance
    git-svn-id: https://svn.apache.org/repos/asf/avro/trunk@1070613 13f79535-47bb-0310-9956-ffa450edef68

--- lang/java/avro/src/main/java/org/apache/avro/io/BinaryDecoder.java
+++ lang/java/avro/src/main/java/org/apache/avro/io/BinaryDecoder.java
@@ -152,41 +152,62 @@ public class BinaryDecoder extends Decoder {
  if (b > 0x7f) {
  b = buf[pos + len++] & 0xff;
  n ^= (b & 0x7f) << 28;
- }
  if (b > 0x7f) {
  throw new IOException("Invalid int encoding");
  }
  }
  }
  }
- if (pos + len > limit) {
- throw new EOFException();
  }
  pos += len;
+ if (pos > limit) {
+ throw new EOFException();
+ }
  return (n >>> 1) ^ -(n & 1); // back to two's-complement
  }
 
  @Override
  public long readLong() throws IOException {
  ensureBounds(10);
- int len = 1;
- int b = buf[pos] & 0xff;
+ int b = buf[pos++] & 0xff;
  int n = b & 0x7f;
  long l;
  if (b > 0x7f) {
- b = buf[pos + len++] & 0xff;
+ b = buf[pos++] & 0xff;
  n ^= (b & 0x7f) << 7;
  if (b > 0x7f) {
- b = buf[pos + len++] & 0xff;
+ b = buf[pos++] & 0xff;
  n ^= (b & 0x7f) << 14;
  if (b > 0x7f) {
- b = buf[pos + len++] & 0xff;
+ b = buf[pos++] & 0xff;
  n ^= (b & 0x7f) << 21;
+ if (b > 0x7f) {
  // only the low 28 bits can be set, so this won't carry
  // the sign bit to the long
+ l = innerLongDecode((long)n);
+ } else {
  l = n;
- if (b > 0x7f) {
- b = buf[pos + len++] & 0xff;
+ }
+ } else {
+ l = n;
+ }
+ } else {
+ l = n;
+ }
+ } else {
+ l = n;
+ }
+ if (pos > limit) {
+ throw new EOFException();
+ }
+ return (l >>> 1) ^ -(l & 1); // back to two's-complement
+ }
+
+ // splitting readLong up makes it faster because of the JVM does more
+ // optimizations on small methods
+ private long innerLongDecode(long l) throws IOException {
+ int len = 1;
+ int b = buf[pos] & 0xff;
  l ^= (b & 0x7fL) << 28;
  if (b > 0x7f) {
  b = buf[pos + len++] & 0xff;
@@ -203,7 +224,6 @@ public class BinaryDecoder extends Decoder {
  if (b > 0x7f) {
  b = buf[pos + len++] & 0xff;
  l ^= (b & 0x7fL) << 63;
- }
  if (b > 0x7f) {
  throw new IOException("Invalid long encoding");
  }
@@ -212,20 +232,8 @@ public class BinaryDecoder extends Decoder {
  }
  }
  }
- } else {
- l = n;
- }
- } else {
- l = n;
- }
- } else {
- l = n;
- }
- if (pos + len > limit) {
- throw new EOFException();
- }
  pos += len;
- return (l >>> 1) ^ -(l & 1); // back to two's-complement
+ return l;
  }
 
  @Override
@@ -261,8 +269,10 @@ public class BinaryDecoder extends Decoder {
  public Utf8 readString(Utf8 old) throws IOException {
  int length = readInt();
  Utf8 result = (old != null ? old : new Utf8());
- result.setLength(length);
+ result.setByteLength(length);
+ if (0 != length) {
  doReadBytes(result.getBytes(), 0, length);
+ }
  return result;
  }
 
