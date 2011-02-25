2cf3b0676235e55dca57eb6d0c17df9effd6d52d
Douglas Adam Creager <dcreager@apache.org>
2011-02-25 16:08:41 +0000

    AVRO-463. C: Error messages
    This patch adds a new avro_strerror() function, which returns a string
    describing the most recent Avro error. Every function that can return
    an error code has been updated to provide an error string. In
    particular, the avro_schema_from_json function now fills in the Avro
    error string with any parse errors that result from an invalid JSON
    string.
    
    git-svn-id: https://svn.apache.org/repos/asf/avro/trunk@1074608 13f79535-47bb-0310-9956-ffa450edef68

--- lang/c/src/avro_errors.h
+++ lang/c/src/avro_errors.h
@@ -14,48 +14,16 @@
  * implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-#ifndef AVRO_PRIVATE_H
-#define AVRO_PRIVATE_H
 
-#include <errno.h>
+#ifndef AVRO_ERRORS_H
+#define AVRO_ERRORS_H
 
 #include "avro.h"
 
-#ifdef HAVE_CONFIG_H
-/* This is only true for now in the autotools build */
-#include "config.h"
-#endif
+void
+avro_set_error(const char *fmt, ...);
 
-#define check(rval, call) { rval = call; if(rval) return rval; }
-
-#define check_set(rval, call, ...) \
-	{ \
- rval = call; \
- if (rval) { \
- avro_set_error(__VA_ARGS__);	\
- } \
-	}
-
-#define check_prefix(rval, call, ...) \
-	{ \
- rval = call; \
- if (rval) { \
- avro_prefix_error(__VA_ARGS__);	\
- } \
-	}
-
-#define check_param(result, test, name) \
-	{ \
- if (!(test)) { \
- avro_set_error("Invalid " name " in %s",	\
- __func__); \
- return result; \
- } \
-	}
-
-#define AVRO_UNUSED(var) (void)var;
-
-#define container_of(ptr_, type_, member_) \
- ((type_ *)((char *)ptr_ - (size_t)&((type_ *)0)->member_))
+void
+avro_prefix_error(const char *fmt, ...);
 
 #endif
