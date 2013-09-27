b8cf9813765420a1f56ce8b8066100bbd0224b68
Doug Cutting <cutting@apache.org>
2013-09-27 20:27:15 +0000

    AVRO-1332. C#: Improve DatumReader performance. Contributed by David McIntosh.
    git-svn-id: https://svn.apache.org/repos/asf/avro/trunk@1527061 13f79535-47bb-0310-9956-ffa450edef68

--- lang/csharp/src/apache/main/Specific/ObjectCreator.cs
+++ lang/csharp/src/apache/main/Specific/ObjectCreator.cs
@@ -314,12 +314,15 @@ namespace Avro.Specific
  NameCtorKey key = new NameCtorKey(name, schemaType);
 
  CtorDelegate ctor;
+ lock(ctors)
+ {
  if (!ctors.TryGetValue(key, out ctor))
  {
  Type type = GetType(name, schemaType);
  ctor = GetConstructor(name, schemaType, type);
 
  ctors.Add(key, ctor);
+ }
  }
  return ctor();
  }
