08a08e4a30bdf6c7bf241dbdb0c338c11cd6961c
Ryo Okubo <syucream1031@gmail.com>
2019-04-04 17:43:49 +0900

    AVRO-2332: Support java_multiple_files protobuf option
    Support java_multiple_files option,
    https://developers.google.com/protocol-buffers/docs/proto3#options
    
    Protoc doesn't generate outer class if the option enables, but current getNamespace() doesn't consider that.

--- lang/java/protobuf/src/main/java/org/apache/avro/protobuf/ProtobufData.java
+++ lang/java/protobuf/src/main/java/org/apache/avro/protobuf/ProtobufData.java
@@ -219,7 +219,8 @@ public class ProtobufData extends GenericData {
  private String getNamespace(FileDescriptor fd, Descriptor containing) {
  FileOptions o = fd.getOptions();
  String p = o.hasJavaPackage() ? o.getJavaPackage() : fd.getPackage();
- String outer;
+ String outer = "";
+ if (!o.getJavaMultipleFiles()) {
  if (o.hasJavaOuterClassname()) {
  outer = o.getJavaOuterClassname();
  } else {
@@ -227,12 +228,19 @@ public class ProtobufData extends GenericData {
  outer = outer.substring(0, outer.lastIndexOf('.'));
  outer = toCamelCase(outer);
  }
+ }
  StringBuilder inner = new StringBuilder();
  while (containing != null) {
- inner.insert(0, "$" + containing.getName());
+ if (inner.length() == 0) {
+ inner.insert(0, containing.getName());
+ } else {
+ inner.insert(0, containing.getName() + "$");
+ }
  containing = containing.getContainingType();
  }
- return p + "." + outer + inner;
+ String d1 = (!outer.isEmpty() || inner.length() != 0 ? "." : "");
+ String d2 = (!outer.isEmpty() && inner.length() != 0 ? "$" : "");
+ return p + d1 + outer + d2 + inner;
  }
 
  private static String toCamelCase(String s) {
