05dc7932c3cb1daaaa7e743b2337dbe4c80ddff1
Thiruvalluvan M G <thiru@startsmartlabs.com>
2019-04-18 18:32:36 -0700

    Applied splotless for reformatting sources

--- lang/java/avro/src/test/java/org/apache/avro/io/TestValidatingIO.java
+++ lang/java/avro/src/test/java/org/apache/avro/io/TestValidatingIO.java
@@ -297,11 +297,12 @@ public class TestValidatingIO {
  bvi = new JsonDecoder(sc, in);
  }
  Decoder vi = new ValidatingDecoder(sc, bvi);
- String msg = "Error in validating case: "+sc;
+ String msg = "Error in validating case: " + sc;
  check(msg, vi, calls, values, skipLevel);
  }
 
- public static void check(Decoder vi, String calls, Object[] values, final int skipLevel) throws IOException {
+ public static void check(String msg, Decoder vi, String calls, Object[] values, final int skipLevel)
+ throws IOException {
  InputScanner cs = new InputScanner(calls.toCharArray());
  int p = 0;
  int level = 0;
@@ -326,12 +327,14 @@ public class TestValidatingIO {
  assertEquals(msg, values[p++], vi.readLong());
  break;
  case 'F':
- if (!(values[p] instanceof Float)) fail();
+ if (!(values[p] instanceof Float))
+ fail();
  float f = (Float) values[p++];
  assertEquals(msg, f, vi.readFloat(), Math.abs(f / 1000));
  break;
  case 'D':
- if (!(values[p] instanceof Double)) fail();
+ if (!(values[p] instanceof Double))
+ fail();
  double d = (Double) values[p++];
  assertEquals(msg, d, vi.readDouble(), Math.abs(d / 1000));
  break;
@@ -364,13 +367,11 @@ public class TestValidatingIO {
  byte[] bb = (byte[]) values[p++];
  ByteBuffer bb2 = vi.readBytes(null);
  byte[] actBytes = new byte[bb2.remaining()];
- System.arraycopy(bb2.array(), bb2.position(), actBytes,
- 0, bb2.remaining());
+ System.arraycopy(bb2.array(), bb2.position(), actBytes, 0, bb2.remaining());
  assertArrayEquals(msg, bb, actBytes);
  }
  break;
- case 'f':
- {
+ case 'f': {
  int len = extractInt(cs);
  if (level == skipLevel) {
  vi.skipFixed(len);
@@ -383,8 +384,7 @@ public class TestValidatingIO {
  }
  }
  break;
- case 'e':
- {
+ case 'e': {
  int e = extractInt(cs);
  if (level == skipLevel) {
  vi.readEnum();
@@ -417,14 +417,14 @@ public class TestValidatingIO {
  }
  case ']':
  assertEquals(msg, 0, counts[level]);
- if (! isEmpty[level]) {
+ if (!isEmpty[level]) {
  assertEquals(msg, 0, vi.arrayNext());
  }
  level--;
  break;
  case '}':
  assertEquals(0, counts[level]);
- if (! isEmpty[level]) {
+ if (!isEmpty[level]) {
  assertEquals(msg, 0, vi.mapNext());
  }
  level--;
@@ -442,8 +442,7 @@ public class TestValidatingIO {
  case 'c':
  extractInt(cs);
  continue;
- case 'U':
- {
+ case 'U': {
  int idx = extractInt(cs);
  assertEquals(msg, idx, vi.readIndex());
  continue;
@@ -461,12 +460,11 @@ public class TestValidatingIO {
  assertEquals(msg, values.length, p);
  }
 
- private static int skip(String msg, InputScanner cs, Decoder vi, boolean isArray)
- throws IOException {
+ private static int skip(String msg, InputScanner cs, Decoder vi, boolean isArray) throws IOException {
  final char end = isArray ? ']' : '}';
  if (isArray) {
  assertEquals(msg, 0, vi.skipArray());
- } else if (end == '}'){
+ } else if (end == '}') {
  assertEquals(msg, 0, vi.skipMap());
  }
  int level = 0;
