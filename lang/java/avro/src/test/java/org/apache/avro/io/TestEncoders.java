5a3ef398fe31eba162041f078f9987abd332ac83
Juan Sebastián Urrego Escobar <juancho088@gmail.com>
2019-08-01 20:57:06 +0200

    Add some tests to validate the include namespace functionality

--- lang/java/avro/src/test/java/org/apache/avro/io/TestEncoders.java
+++ lang/java/avro/src/test/java/org/apache/avro/io/TestEncoders.java
@@ -17,25 +17,11 @@
  */
 package org.apache.avro.io;
 
-import static java.util.Arrays.asList;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.apache.avro.AvroTypeException;
 import org.apache.avro.Schema;
 import org.apache.avro.Schema.Type;
@@ -45,6 +31,24 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class TestEncoders {
  private static final int ENCODER_BUFFER_SIZE = 32;
@@ -123,6 +127,32 @@ public class TestEncoders {
  writer.write(2, e);
  e.flush();
  Assert.assertEquals("1" + separator + "2", out.toString());
+ }
+
+ @Test
+ public void testJsonEncoderWhenIncludeNamespaceOptionIsFalse() throws IOException {
+ String value = "{\"b\": {\"string\":\"myVal\"}, \"a\": 1}";
+ String schemaStr = "{\"type\": \"record\", \"name\": \"ab\", \"fields\": ["
+ + "{\"name\": \"a\", \"type\": \"int\"}, {\"name\": \"b\", \"type\": [\"null\", \"string\"]}" + "]}";
+ Schema schema = new Schema.Parser().parse(schemaStr);
+ byte[] avroBytes = fromJsonToAvro(value, schema);
+ ObjectMapper mapper = new ObjectMapper();
+
+ assertEquals(mapper.readTree("{\"b\":\"myVal\",\"a\":1}"),
+ mapper.readTree(fromAvroToJson(avroBytes, schema, false)));
+ }
+
+ @Test
+ public void testJsonEncoderWhenIncludeNamespaceOptionIsTrue() throws IOException {
+ String value = "{\"b\": {\"string\":\"myVal\"}, \"a\": 1}";
+ String schemaStr = "{\"type\": \"record\", \"name\": \"ab\", \"fields\": ["
+ + "{\"name\": \"a\", \"type\": \"int\"}, {\"name\": \"b\", \"type\": [\"null\", \"string\"]}" + "]}";
+ Schema schema = new Schema.Parser().parse(schemaStr);
+ byte[] avroBytes = fromJsonToAvro(value, schema);
+ ObjectMapper mapper = new ObjectMapper();
+
+ assertEquals(mapper.readTree("{\"b\":{\"string\":\"myVal\"},\"a\":1}"),
+ mapper.readTree(fromAvroToJson(avroBytes, schema, true)));
  }
 
  @Test
@@ -259,5 +289,37 @@ public class TestEncoders {
  result[0] = (byte) (bytes.length * 2); // zig-zag encoding
  System.arraycopy(bytes, 0, result, 1, bytes.length);
  return result;
+ }
+
+ private byte[] fromJsonToAvro(String json, Schema schema) throws IOException {
+ DatumReader<Object> reader = new GenericDatumReader<>(schema);
+ GenericDatumWriter<Object> writer = new GenericDatumWriter<>(schema);
+ ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+ Decoder decoder = DecoderFactory.get().jsonDecoder(schema, json);
+ Encoder encoder = EncoderFactory.get().binaryEncoder(output, null);
+
+ Object datum = reader.read(null, decoder);
+
+ writer.write(datum, encoder);
+ encoder.flush();
+
+ return output.toByteArray();
+ }
+
+ private String fromAvroToJson(byte[] avroBytes, Schema schema, boolean includeNamespace) throws IOException {
+ GenericDatumReader<Object> reader = new GenericDatumReader<>(schema);
+ DatumWriter<Object> writer = new GenericDatumWriter<>(schema);
+ ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+ JsonEncoder encoder = factory.jsonEncoder(schema, output);
+ encoder.setIncludeNamespace(includeNamespace);
+ Decoder decoder = DecoderFactory.get().binaryDecoder(avroBytes, null);
+ Object datum = reader.read(null, decoder);
+ writer.write(datum, encoder);
+ encoder.flush();
+ output.flush();
+
+ return new String(output.toByteArray(), StandardCharsets.UTF_8.name());
  }
 }
