81f0659b5598a6dc64625be7670a9c66dc2597e0
Stephan Lachnit <stephanlachnit@debian.org>
2026-01-22 10:18:08 +0100

    AVRO-4221: [C++] Allow using symbol visibility annotations also on non-Windows platforms (#3600)
    In GCC and clang the symbol visibility behavior of MSVC can be mirror using `-fvisibility=hidden`.
    This allows to more easily test that symbol visbility annotations work correctly and can potentially lead to smaller binaries.
    The default behavior on non-Windows platforms is not changed with this commit.
    
    See also https://gcc.gnu.org/wiki/Visibility
    
    Also fixes a tiny mistake where the _WIN32 macro was used to silence a MSVC warning where the _MSC_VER macro should have been used instead.

--- lang/c++/include/avro/Compiler.hh
+++ lang/c++/include/avro/Compiler.hh
@@ -26,15 +26,15 @@
 
 namespace avro {
 
-class AVRO_DECL InputStream;
+class InputStream;
 
 /// This class is used to implement an avro spec parser using a flex/bison
 /// compiler. In order for the lexer to be reentrant, this class provides a
 /// lexer object for each parse. The bison parser also uses this class to
 /// build up an avro parse tree as the avro spec is parsed.
 
-class AVRO_DECL Name;
-class AVRO_DECL ValidSchema;
+class Name;
+class ValidSchema;
 
 /// Given a stream containing a JSON schema, compiles the schema to a
 /// ValidSchema object. Throws if the schema cannot be compiled to a valid
